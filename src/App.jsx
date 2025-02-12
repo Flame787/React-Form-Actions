b718ed68c2664515741ce57d2e389f61a8dbe3f8
Flame787 <mbrezovic77@gmail.com>
2025-02-12 20:25:33 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from './components/Header.jsx';
+import Signup from './components/Signup.jsx';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Signup />
+      </main>
+    </>
+  );
+}
+
+export default App;
+
+
+// React Form Actions - feature built in React, if we are using React version 19 or higher
+
+// - used instead of handling these actions manually, like in section 17 (before this one)
